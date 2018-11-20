d699a7c5f5dcadab471bc6fabdaaedbefcfbd9c9
JoelSpinelli <joel.spinelli@hotmail.com>
2018-11-19 22:20:24 -0200

    Melhorias de CSS e Limpar Formulario

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -18,6 +18,7 @@ export default class Todo extends Component {
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
 
         this.refresh()
     }
@@ -51,6 +52,10 @@ export default class Todo extends Component {
         this.refresh(this.state.description)
     }
 
+    handleClear() {
+        this.refresh()
+    }
+
     refresh(description = '') {
         const search = description ? `&description__regex=/${description}/` : ''
         axios.get(`${URL}?sort=-createdAt${search}`)
@@ -65,6 +70,7 @@ export default class Todo extends Component {
                     handleChange={this.handleChange}
                     description={this.state.description}
                     handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}
                     handleAdd={this.handleAdd} />
                 <TodoList 
                     list={this.state.list}

15530b1158c7083ba6e65bc692739b31476077e4
JoelSpinelli <joel.spinelli@hotmail.com>
2019-01-16 22:32:56 -0200

    Exclusao de TODOs

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -35,3 +35,10 @@ export const markAsPending = (todo) => {
       .then(resp => dispatch(search()))
   }
 }
+
+export const remove = (todo) => {
+  return dispatch => {
+    axios.delete(`${URL}/${todo._id}`)
+      .then(resp => dispatch(search()))
+  }
+}

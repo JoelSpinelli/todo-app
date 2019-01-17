5a3115da5c0ea79d96acfaa9aec3779241976236
JoelSpinelli <joel.spinelli@hotmail.com>
2019-01-16 22:40:20 -0200

    limpar formulário

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux'
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
-import { add, changeDescription, search } from './todoActions'
+import { add, changeDescription, search, clear } from './todoActions'
 
 class TodoForm extends Component {
     constructor(props) {
@@ -21,7 +21,7 @@ class TodoForm extends Component {
         if(e.key === 'Enter') {
             e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
-            this.props.handleClear()
+            this.props.clear()
         }
     }
 
@@ -43,7 +43,7 @@ class TodoForm extends Component {
                         <IconButton style='info' icon='search'
                             onClick={() => search()}></IconButton>
                         <IconButton style='default' icon='close'
-                            onClick={this.props.handleClear}></IconButton>
+                            onClick={this.props.clear}></IconButton>
                     </Grid>
                 </div>
             </div>
@@ -52,5 +52,5 @@ class TodoForm extends Component {
 }
 
 const mapStateToProps = state => ({description: state.todo.description})
-const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ add, changeDescription, search, clear }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
